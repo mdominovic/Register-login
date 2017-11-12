fffcb5dfa0916b92fed5d3396c2bab4b43fcc5f3
mdominovic <mdominovic1@gmail.com>
2017-11-12 15:25:54 +0100

    first

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'core/init.php';
+
+if(Session::exists('home')){
+    echo '<p>' . Session::flash('home') . '</p>';
+}
+
+$user = new User();
+
+if($user->isLoggedIn()) {
+?>
+    <p>Hello <a href="#"><?php echo escape($user->data()->username); ?></a>!</p>
+    
+    <ul>
+        <li><a href="logout.php">Log out</a></li>    
+    </ul>
+    
+<?php                     
+} else {
+    echo '<p>You need to  <a href="login.php">log in</a> or  <a href="register.php">register</a></p>';
+}
